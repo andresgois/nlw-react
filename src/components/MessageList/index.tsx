9c0c68083ba2235a950c6a370a544f71f6d2dcef
andre gois <andre.s.gois3@gmail.com>
2021-10-24 19:20:14 -0300

    consuming the backend message route

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,49 +1,52 @@
 import styles from './style.module.scss';
+import { api } from '../../services/api';
 
 import logo from '../../assets/logo.svg';
+import { useEffect, useState } from 'react';
+
+type Message = {
+    id: string;
+    text: string;
+    user: {
+        name: string;
+        avatar_url: string;
+    }
+};
 
 export function MessageList(){
+
+    const [messages, setMessages ] = useState<Message[]>([]);
+
+    useEffect(() => {
+        // chamada pata api
+        api.get<Message[]>('/messages/getLast3').then(response => {
+            setMessages(response.data)
+        })
+    },[]);
+
     return (
         <div className={styles.messageListWrapper}>
             <img src={logo} alt="Do while" />
 
             <ul className={styles.messageList}>
 
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Não vejo a hora de começa o Do While
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/andresgois.png" alt="Foto" />
-                        </div>
-                        <span>André Gois</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Não vejo a hora de começa o Do While
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/andresgois.png" alt="Foto" />
-                        </div>
-                        <span>André Gois</span>
-                    </div>
-                </li>
-
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>
-                        Não vejo a hora de começa o Do While
-                    </p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/andresgois.png" alt="Foto" />
-                        </div>
-                        <span>André Gois</span>
-                    </div>
-                </li>
+                {
+                    messages.map( message => {
+                        return (
+                            <li key={message.id} className={styles.message}>
+                                <p className={styles.messageContent}>
+                                    {message.text}
+                                </p>
+                                <div className={styles.messageUser}>
+                                    <div className={styles.userImage}>
+                                        <img src={message.user.avatar_url} alt={message.user.name} />
+                                    </div>
+                                    <span>{message.user.name}</span>
+                                </div>
+                            </li>
+                        )
+                    })
+                }
 
             </ul>
         </div>
